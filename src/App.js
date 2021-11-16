4df219cdc462d79ae55c278fa36bf2057e8a0f7a
jmunoz94 <jesus.munoz@bairesdev.com>
2021-11-15 19:44:35 -0500

    Added routes, updated the general structure of the app and removed some unnecessary stuff (some more is still there)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+import CreateProductForm from "./components/createProduct";
+import CreateSalesOrderForm from "./components/createSalesOrder";
+import ListProducts from "./components/readProduct";
+import ListSalesOrders from "./components/readSalesOrder";
+import { WrappedUpdateProductForm } from "./components/updateProduct";
+import { WrappedUpdateSalesOrderForm } from "./components/updateSalesOrder";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sales Order Management System</h1>
+      <nav>
+        <a href="/products">Products</a>
+        <a href="/sales-orders">Sales Orders</a>
+        <a href="/accounts">Accounts</a>
+      </nav>
+      <div>
+        <Routes>
+          <Route exact path="/products" element={<ListProducts />} />
+          <Route exact path="/products/add" element={<CreateProductForm />} />
+          <Route path="/products/:id" element={<WrappedUpdateProductForm />} />
+          <Route path="/sales-orders" element={<ListSalesOrders />} />
+          <Route path="/sales-orders/add" element={<CreateSalesOrderForm />} />
+          <Route
+            path="/sales-orders/:id"
+            element={<WrappedUpdateSalesOrderForm />}
+          />
+        </Routes>
+      </div>
+      <footer>
+        MisionTIC 2021
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
